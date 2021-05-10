32d48bb4222665d997a5bdcfcdcbcc92960715e7
sahilrazz <65617655+sahilrazz@users.noreply.github.com>
2021-05-10 22:31:29 +0530

    website beccame responsive

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     $('.slider').slick({
-        arrows:false,
-        dots:true,
-        appendDots:'.slider-dots',
-        dotsClass:'dots',
-// auto run ________________________
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots',
+        // auto run ________________________
         autoplay: true,
         autoplaySpeed: 1000,
         pauseOnFocus: false,
@@ -15,4 +15,19 @@ $(document).ready(function () {
         fade: true,
         cssEase: 'linear'
     });
+
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+    hamberger.addEventListener('click',function(){
+        mobileNav.classList.add('open');
+
+
+    });
+    times.addEventListener('click',function(){
+        mobileNav.classList.remove('open');
+    });
 });
+
+
